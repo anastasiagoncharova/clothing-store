add935bca1816864ccf38a74ec890635f2882c02
etecture-agoncharova <goncharova_ana@ukr.net>
2023-01-25 14:00:47 +0200

    fix: add useCallback

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,32 +1,38 @@
-import {CartDropdownContainer, EmptyMessage, CartItems} from './cart-dropdown.styles.jsx';
-import Button from '../button/button.component';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import {selectCartItems} from '../../store/cart/cart.selector';
+import { useCallback } from "react";
 
-import CartItem from '../cart-item/cart-item.component';
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
+import { selectCartItems } from "../../store/cart/cart.selector";
+
+import {
+  CartDropdownContainer,
+  EmptyMessage,
+  CartItems,
+} from "./cart-dropdown.styles";
 
 const CartDropdown = () => {
   const cartItems = useSelector(selectCartItems);
   const navigate = useNavigate();
 
-  const gotoCheckoutHandler = () => {
-    navigate('/checkout');
-  }
+  const goToCheckoutHandler = useCallback(() => {
+    navigate("/checkout");
+  }, []);
+
   return (
     <CartDropdownContainer>
       <CartItems>
-        {
-          cartItems.length ? (cartItems.map(item => (
-            <CartItem key={item.id} cartItem={item} />
-          ))) : (
-            <EmptyMessage>Your cart is empty</EmptyMessage>
-          )
-        }
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <EmptyMessage>Your cart is empty</EmptyMessage>
+        )}
       </CartItems>
-      <Button onClick={gotoCheckoutHandler}>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </CartDropdownContainer>
-  )
-}
+  );
+};
 
 export default CartDropdown;
